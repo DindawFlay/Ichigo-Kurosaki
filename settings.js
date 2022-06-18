faf6b9f1f458e6964b0e28813447d7c36f1a0a91
DindawFly <97446180+DindawFlay@users.noreply.github.com>
2022-06-19 00:55:05 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,11 +9,11 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //Ubah Disini
-global.ownerNumber = ['6285609233482','6287877173955','0']
-global.ownerName = 'Fandyyy X All Creator'
-global.packname = 'Ichigo Kurosaki'
-global.author = 'Whatsapp Bot 2022'
-global.prefa = ['','!','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
+global.ownerNumber = ['6289515233398','0']
+global.ownerName = 'Sesilla'
+global.packname = 'SesillaNBL'
+global.author = 'Sesilla'
+global.prefa = ['','.','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'sesi'
 
 //Message Nya Ubah Disini
